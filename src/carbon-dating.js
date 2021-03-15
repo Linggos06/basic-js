240fd9a8463274d3d1d2e9fecb5324ff65470a63
Julia Sen <julia.snk@hotmail.com>
2021-03-16 01:01:56 +0300

    solve count-cats and carbon-dating tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,15 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(data) {
+ 
+  if(typeof data === "string" && Number(data) > 0) {
+    const res =  Math.ceil(Math.log(MODERN_ACTIVITY / Number(data)) / (0.693 /HALF_LIFE_PERIOD ));
+    if(res < 0) {
+      return false;
+    }else{
+      return res;
+    }
+   }
+   return false;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(array) {
+
+  const merged = [].concat(...array);
+  return merged.reduce((acc, val) => {
+         if(val === '^^'){
+          return acc += 1;
+         }
+         return acc;
+  }, 0);
+  
 };
