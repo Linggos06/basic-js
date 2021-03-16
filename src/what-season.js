e0559e2eb23d1dfbcfdd39fbb16f9e4b239f3bb4
Julia Sen <julia.snk@hotmail.com>
2021-03-16 17:39:03 +0300

    try to solve what-season 2

--- src/what-season.js
+++ src/what-season.js
@@ -2,27 +2,14 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(input) {
   
-  if(!input instanceof Date || !input){
+  if(Object.prototype.toString.call(input) !== '[object Date]'){
     return 'Unable to determine the time of year!';
   }
 
-  const day = input.getDate();
-  const month = input.getMonth();
-  const year = input.getFullYear();
-  const daysPerMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-  if (year % 4 == 0) {
-    daysPerMonth[1] = 29;
-}
-
-if (day > daysPerMonth[month]) {
-  return false;
-}
-    
-   const monthh = input.getMonth();
+   const month = input.getMonth();
    let season = '';
     
-   switch(monthh) {
+   switch(month) {
       case 11:
       case 0:
       case 1:
