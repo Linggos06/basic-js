93165912659d405ff950d69c15d81c52047b156a
Julia Sen <julia.snk@hotmail.com>
2021-03-17 15:39:01 +0300

    solve transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+
+  if (!Array.isArray(arr)){
+    throw err;
+  }
+
+  const next = "--discard-next";
+  const prev = "--discard-prev";
+  const dnext = "--double-next";
+  const dprev = "--double-prev";
+
+  const newArr = [];
+  const copyArr = arr.map(x => x);
+
+  for(let i = 0; i < copyArr.length; i++){
+    
+    if(copyArr[i - 1] === next){
+      continue;
+    }
+     if (copyArr[i - 1] === dnext){
+      newArr.push(copyArr[i]);
+    } 
+    
+    if(copyArr[i + 1] === prev){
+      continue;
+    }
+    
+    if(copyArr[i + 1] === dprev){
+        newArr.push(copyArr[i]);
+    }
+    
+    if(copyArr[i] !== next && copyArr[i] !== prev && copyArr[i] !== dnext && copyArr[i] !== dprev){
+    newArr.push(copyArr[i]);
+    }
+
+  }
+  
+  return newArr;
 };
