35d5737d7ea330b1c9c3b4e60d1d36e9cf2c3dff
Linggos06 <julia.snk@hotmail.com>
2021-03-19 19:40:03 +0300

    solve recursive depth task

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,16 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
- 
-  calculateDepth(array, depth=1) {
-    
-    for (const subArr of array) {
-      if(Array.isArray(subArr)){
-        return depth + this.calculateDepth(subArr);
+
+  calculateDepth(array, depth = 1) {
+
+
+    if (!Array.isArray(array)) throw new Exception('not an array');
+
+    let res = depth;
+    for (let i = 0; i < array.length; ++i) {
+      if (Array.isArray(array[i])) {
+        let subDepth = this.calculateDepth(array[i], depth + 1);
+        if (subDepth > res) {
+          res = subDepth;
         }
+      }
     }
-      return depth;
+    return res;
+
   }
-  
-  
+
 };
