1ca876b8ca6500a5a998cb753651f376262ee38f
Linggos06 <julia.snk@hotmail.com>
2021-03-21 23:16:07 +0300

    solve vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,107 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(machine){
+    this.machine = machine;
+  }
+  
+  
+  encrypt(string, key) {
+    if(!string || !key){
+      throw new Error();
+    }
+
+    let output = "";
+  
+    function isUppercase(c) {
+      return 65 <= c && c <= 90;  
+    }
+  
+    function isLowercase(c) {
+      return 97 <= c && c <= 122; 
+    }
+
+    function isLetter(c) {
+      return isUppercase(c) || isLowercase(c);
+    }
+
+      let result = [];
+      for (let i = 0; i < key.length; i++) {
+        const c = key.charCodeAt(i);
+        if (isLetter(c))
+          result.push((c - 65) % 32);
+      }
+     
+   
+    for (let i = 0, j = 0; i < string.length; i++) {
+        const c = string.charCodeAt(i);
+        if (isUppercase(c)) {
+            output += String.fromCharCode((c - 65 + result[j % result.length]) % 26 + 65);
+            j++;
+        } else if (isLowercase(c)) {
+            output += String.fromCharCode((c - 97 + result[j % result.length]) % 26 + 97);
+            j++;
+        } else {
+            output += string.charAt(i);
+        }
+    }
+
+    if(this.machine === false && this.machine !== ''){
+      return output.toUpperCase().split('').reverse().join('');
+    }
+    return output.toUpperCase();
+    
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(encryptedMessage, key) {
+     if(!encryptedMessage || !key){
+       throw new Error();
+     }
+     
+     let output = "";
+  
+    function isUppercase(c) {
+      return 65 <= c && c <= 90;  
+    }
+  
+    function isLowercase(c) {
+      return 97 <= c && c <= 122; 
+    }
+
+    function isLetter(c) {
+      return isUppercase(c) || isLowercase(c);
+    }
+
+      let result = [];
+      for (let i = 0; i < key.length; i++) {
+        const c = key.charCodeAt(i);
+        if (isLetter(c))
+          result.push((c - 65) % 32);
+      }
+
+
+      for (var i = 0; i < result.length; i++){
+				result[i] = (26 - result[i]) % 26;
+      }
+   
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+        const c = encryptedMessage.charCodeAt(i);
+        if (isUppercase(c)) {
+            output += String.fromCharCode((c - 65 + result[j % result.length]) % 26 + 65);
+            j++;
+        } else if (isLowercase(c)) {
+            output += String.fromCharCode((c - 97 + result[j % result.length]) % 26 + 97);
+            j++;
+        } else {
+            output += encryptedMessage.charAt(i);
+        }
+    }
+
+
+
+     if(this.machine === false && this.machine !== ''){
+      return output.toUpperCase().split('').reverse().join('');
+    }
+    return output.toUpperCase();
   }
 }
 
